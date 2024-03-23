c1771b72e53a9475eb16395cc4ed43b9e1fe7409
Ticopera20 <luis_ticona1@usmp.pe>
2024-03-23 09:37:13 -0500

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace STORE_PROYECTO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
